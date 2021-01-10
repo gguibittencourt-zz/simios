0f15e5f45bce714edda41d6cfc7962999e572b8d
Guilherme Bittencourt da Silva <guilherme.bittencourt@asksuite.com>
2021-01-10 19:27:06 -0300

    Migrations sequelize

---
+++ config/database/migrations/init.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('dna', {
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+      },
+      chain: Sequelize.DataTypes.JSON,
+      is_simian: Sequelize.DataTypes.BOOLEAN,
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('dna');
+  },
+};
